6fbbba42a62f3afcd75a30b38c0ebf88d5d1deac
CharlyCst <charly.castes@epfl.ch>
2024-04-14 18:19:17 +0200

    Add null call transition data

--- scripts/transitions.py
+++ scripts/transitions.py
@@ -6,6 +6,7 @@ PATH = "../data/ubench/transitions.csv"
 tyche = []
 kvm = []
 raw = []
+null = []
 
 with open(PATH, 'r') as file:
   csvreader = csv.reader(file)
@@ -14,9 +15,11 @@ with open(PATH, 'r') as file:
     tyche.append(float(row[0]))
     raw.append(float(row[1]))
     kvm.append(float(row[2]))
+    null.append(float(row[3]))
     
 
 print(f"sdk-tyche: {np.average(tyche):.5f} +/- {np.std(tyche):.5f} us")
 print(f"kvm:       {np.average(kvm):.5f} +/- {np.std(kvm):.5f} us")
 print(f"raw:       {np.average(raw):.5f} +/- {np.std(raw):.5f} us")
+print(f"null:      {np.average(null):.5f} +/- {np.std(null):.5f} us")
 
